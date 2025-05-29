85324af371a223348af78e6462bc1c10de72b69f
Chaeyeoncho <gcy9293@naver.com>
2025-05-29 14:36:58 +0900

    class refactoring

--- src/class1/ClassStart2.java
+++ src/class1/ClassStart2.java
@@ -2,12 +2,13 @@ package class1;
 
 public class ClassStart2 {
     public static void main(String[] args) {
-        String[] studentsNames = {"학생1", "학생2"}; // 변경 3개 해야함! 인덱스 순서 정확하게 맞춰야함!
-        int[] studentsAges = {15 ,16};
-        int[] studentsGrade = {90, 80};
+        String[] studentName = {"학생1,학생2"};
+        int[] studentAge = {15, 16};
+        int [] studentGrade = {90,80};
 
-        for(int i = 0; i < studentsNames.length; i++){
-            System.out.println("이름" + studentsNames[i] + "나이" + studentsAges[i] + "점수" + studentsGrade[i]);
+        for(int i = 0; i < studentName.length; i++){
+            System.out.println("이름 " + studentName[i] + "나이" + studentAge[i] + "성적" + studentGrade);
         }
     }
 }
+// 항상 배열의 데이터를 잘 맞춰야함 -> 실수 할 가능성 많음 !
--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -2,19 +2,23 @@ package class1;
 
 public class ClassStart3 {
     public static void main(String[] args) {
-        // int > 정수 타입 , string > 문자 타입 그러면 student 라는 타입 만들면 되지 않을까 ?
-        Student student1 = new Student(); // 학생을 실제 메모리에 적재 , 이게 객체(= 인스턴스) student student1은 student는 student의 객체를 받을 수 있는것
-        student1.name = "학생1"; //객체에 접근!
+        Student student1; // student1을 담아둘 수 있는 변수
+        student1 = new Student(); // student클래스에 있는 정보를 변수에 저장
+        student1.name = "학생1";
         student1.age = 15;
         student1.grade = 90;
 
-        Student student2 = new Student();// 학생을 실제 메모리에 적재 , 이게 객체(= 인스턴스)
+        Student student2 = new Student();
         student2.name = "학생2";
-        student2.age = 15;
+        student2.age = 16;
         student2.grade = 80;
 
-        //객체 값 사용
-        System.out.println("이름 : " + student1.name + " 나이" + student1.age + " 성적 " + student1.grade);
-        System.out.println("이름 : " + student2.name + " 나이" + student2.age + " 성적 " + student2.grade);
+        System.out.println("이름 " + student1.name + "나이" + student1.age + "성적" + student1.grade);
+        System.out.println("이름 " + student2.name + "나이" + student2.age + "성적" + student2.grade);
+
+        // 객체는 클래스에서 정의한 속성과 기능을 가진 실체
+        // 예를 들어 student1과 student2는 같은 클래스에서 만들어졌지만 , 서로 다른 객체
+        // 인스턴스는 특정 클래스로 부터 생성된 객체를 의미한다.(뉘앙스의 차이)
+        // student1 객체는 Student클래스의 인스턴스다.(관계가 있을 때)
     }
 }
