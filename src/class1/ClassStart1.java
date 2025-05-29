99113b7a549eda2617e6f4a09309cf3e281dfd12
Chaeyeoncho <gcy9293@naver.com>
2025-05-29 10:12:06 +0900

    the reason why need class

--- src/class1/ClassStart1.java
+++ src/class1/ClassStart1.java
@@ -3,15 +3,18 @@ package class1;
 public class ClassStart1 {
     public static void main(String[] args) {
         String student1Name = "학생1";
-        int student1Age = 15;
+        int studen1tAge = 15;
         int student1Grade = 90;
 
-        String student2Name = "학생1";
-        int student2Age = 15;
+        String student2Name = "학생2";
+        int studen2tAge = 15;
         int student2Grade = 90;
 
-        System.out.println("이름" + student1Name + "나이" + student1Age + "점수" + student1Grade);
-        System.out.println("이름" + student2Name + "나이" + student2Age + "점수" + student2Grade);
+        System.out.println("이름 " + student1Name + "나이" + studen1tAge + "성적" + student1Grade);
+        System.out.println("이름 " + student2Name + "나이" + studen2tAge + "성적" + student2Grade);
 
+        // 이렇게 하면 너무 많은 변수 만들거나 해야함 ..!
+        // 그러면 for문으로 ? -> 하지만 변수 이름 다 다름
+        // 배열로 풀어보자 !
+        }
     }
-}
